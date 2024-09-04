4075b29d91d08a2c74e11a3a15ff1ce113372148
ROHAN KUSHWAHA <kushwaharohan2003@gmail.com>
2024-09-05 00:29:45 +0530

    final commit

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] private Color32 hasPackageColor = new(1, 1, 1, 1);
+    [SerializeField] private Color32 noPackageColor = new(1, 1, 1, 1);
+
     [SerializeField] bool hasPackage;
     [SerializeField] float packageDestroyTime;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log($"Ouch!");
@@ -17,12 +27,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log($"Package Recieved");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor; 
             Destroy(collision.gameObject, packageDestroyTime);
         }
         else if (collision.tag == "Customer" && hasPackage)
         {
             Debug.Log($"Package Delivered"); 
             hasPackage = false;
+            spriteRenderer.color = noPackageColor; 
         }
     }
 }
